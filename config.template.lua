57ef22ff352a6f69d337449dd0cefe4c03fecf81
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2023-08-30 15:53:18 +0500

    Add a file 'config.template.lua'. Move the table `CONFIG` to it. Now it requires to create a local file 'config.lua'

---
+++ config.template.lua
@@ -0,0 +1,33 @@
+return {
+	motor = {
+		vangle = 0,
+		hangle = math.pi/2,
+		velocity = 1,
+		strength = 5000,
+		index = { hmotor = 1, vmotor = 2 },
+		min_vangle = math.rad(10),
+	},
+	target = {
+		position = {-5.25, -0.25, -0.25},
+		velocity = {0, 0, 0},
+		acceleration = {0, 0, 10},
+	},
+	tracker = {
+		expiration_time = 2,
+		min_height = 0.5,
+		min_distance = 8,
+		max_distance = 250,
+		shutter_speed = 0.2,
+		mean = { velocity = 2 / (4 + 1), acceleration = 2 / (15 + 1) },
+	},
+	projectile = {
+		speed = 80,
+		acceleration = 0,
+	},
+	autolaunch = {
+		enable = true,
+		position_samples_number = 15,
+		stabilization_time = 0.15,
+		min_launch_vangle = math.rad(20),
+	},
+}
--- main.lua
+++ main.lua
@@ -39,36 +39,17 @@ function table_to_vec3(x)
 	end
 	return ([[sm.vec3.new(%s)]]):format(x:sub(2, -2):gsub(' ', ''))
 end
+function prequire(m)
+	local ok, err = pcall(require, m) 
+	if not ok then return nil, err end
+	return err
+end
 
-CONFIG = {
-	motor = {
-		vangle = 0,
-		hangle = math.pi/2,
-		velocity = 1,
-		strength = 5000,
-		index = { hmotor = 1, vmotor = 2 },
-		min_vangle = math.rad(10),
-	}, target = {
-		position = {-5.25, -0.25, -0.25},
-		velocity = {0, 0, 0},
-		acceleration = {0, 0, 10}
-	}, tracker = {
-		expiration_time = 2,
-		min_height = 0.5,
-		min_distance = 8,
-		max_distance = 250,
-		shutter_speed = 0.2,
-		mean = { velocity = 2 / (4 + 1), acceleration = 2 / (15 + 1) }
-	}, projectile = {
-		speed = 80,
-		acceleration = 0
-	}, autolaunch = {
-		enable = true,
-		position_samples_number = 15,
-		stabilization_time = 0.15,
-		min_launch_vangle = math.rad(20)
-	},
-}
+CONFIG = prequire("config")
+if not CONFIG then
+	io.stderr:write("ERROR: the config file 'config.lua' was not found. You can create it using the template file 'config.template.lua'\n")
+	os.exit(1)
+end
 )
 sqrt, sin, cos, pi, acos, asin = math.sqrt, math.sin, math.cos, math.pi, math.acos, math.asin
 
